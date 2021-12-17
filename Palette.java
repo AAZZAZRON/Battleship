c9775b188f5bb4fa95100e49b7083e8bf187247b
Maplefin <81708250+Maplefin444@users.noreply.github.com>
2021-12-17 14:04:08 -0500

    added colors
    idk if we need the constructor since java creates a default constructor for all classes

--- Palette.java
+++ Palette.java
@@ -1,7 +1,29 @@
-// The "Palette" class.
-import java.awt.*;
-import hsa.Console;
+/*
+* James Huynh and Aaron Zhu
+* Ms. Krasteva
+* ISP ICS3UP
+* This class contains all of the colors needed in other classes.
+* Dec 17, 2021
 
-public class Palette {
+Variable Name         Type         Description
+SHIP_GRAY             Color        Stores the associated color.
+SKY_BLUE              Color        Stores the associated color.
+OCEAN_BLUE            Color        Stores the associated color.
+CONSOLE_GRAY          Color        Stores the associated color.
+TEXT_GREEN            Color        Stores the associated color.
+MARKER_RED            Color        Stores the associated color.
+*/
 
+public class Palette
+{
+    final Color SHIP_GRAY = new Color(153, 153, 153);
+    final Color SKY_BLUE = new Color(164, 194, 244);
+    final Color OCEAN_BLUE = new Color(109, 158, 235);
+    final Color CONSOLE_GRAY = new Color(183, 183, 183);
+    final Color TEXT_GREEN = new Color(0, 255, 0);
+    final Color MARKER_RED = new Color(204,0,0);
+
+
+    public Palette(){
+    }
 }
