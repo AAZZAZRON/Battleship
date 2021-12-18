6c388d4588fb1800435f586be4494943326b8ae0
Maplefin <james.huynh444@gmail.com>
2021-12-17 19:46:23 -0500

    added splash_waves

---
+++ Splash_Waves.java
@@ -0,0 +1,18 @@
+/*
+* James Huynh and Aaron Zhu
+* Ms. Krasteva
+* ISP ICS3UP
+* This class contains 
+* Dec 17, 2021
+
+Variable Name         Type         Description
+c                   Console        Same console as the one in Battleship.java, as it is passed through the constructor
+*/
+
+import java.awt.*;
+import hsa.Console;
+
+public class Splash_Waves
+{
+    private Console c;
+}
