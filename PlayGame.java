343ff74290e197a0f899d664e0d493a6f8b1ada9
Aaron Zhu <aaronzhu3.1415@gmail.com>
2021-12-17 22:16:55 -0500

    generate user ships

--- PlayGame.java
+++ PlayGame.java
@@ -34,7 +34,15 @@ public class PlayGame
 	
 	drawBackground();
 
-	user.generateShips(); // generate user ships
+	user.generateShips(5); // generate user ships
+	
+	for (int i = 0; i < 10; i += 1) {
+	    for (int j = 0; j < 10; j += 1) System.out.print(user.hasShip[i][j] + "\t");
+	    System.out.println();
+	}
+	    
+	
+	
 	enemy.generateShips(); // generate enemy ships
 	
 	while (!finished) { // while the game isn't finished
